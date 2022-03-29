56d0f80078ab2a946da15071f786744a588d0c8e
Sir-Aguiar <felipeferreiraaguiar9@gmail.com>
2022-03-29 19:36:41 -0400

    Form pre-build

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { BsFillSunFill, BsFillMoonFill } from "react-icons/bs";
 import "./styles/App.css";
 const App: React.FC = () => {
   const [darkMode, setDark] = useState(false);
+  /* const [userType, setUsertype] = useState<"Professor" | "Student">("Student"); */
   useEffect(() => {
     const body = document.getElementById("body") as HTMLBodyElement;
     if (darkMode) {
@@ -20,7 +21,27 @@ const App: React.FC = () => {
       >
         {darkMode ? <BsFillSunFill /> : <BsFillMoonFill />}
       </span>
-      <form className="mainFormulary"></form>
+      <form className="mainFormulary">
+        <div className="userType">
+          <div className="studentWrapper wrapper">
+            <div className="studentModule bg-student"></div>
+            <span className="wrapperCaption">Professor</span>
+          </div>
+          <div className="professorWrapper wrapper">
+            <div className="professorModule "></div>
+            <span className="wrapperCaption">Aluno</span>
+          </div>
+        </div>
+        <div className="username inputField">
+          <input type="text" name="username" id="username" placeholder="Usuário" />
+        </div>
+        <div className="userpassword inputField">
+          <input type="password" name="password" id="password" placeholder="Senha" />
+        </div>
+        <div className="submitArea">
+          <button type="submit">Entrar</button>
+        </div>
+      </form>
     </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,25 +10,28 @@ module.exports = {
       colors: {
         "l": {
           "bg": "#D7C7E3",
-          "test-color": "#A63EC5",
-          "formulary-color": "#7D39C6",
-          "formulary-color2": "#7D34CB",
-          "formulary-button": "#7D42BD",
-          "formulary-color3": "#9768CA",
+          'form-bg': "rgb(238,238,238,.97)",
+          "formulary-color": "#4c1b9b",
         },
         "d": {
           "bg": "#1B2350",
+          'form-bg': "#3b3643",
+          "formulary-color": "#7D39C6",
           "secundary-text": "#F2EFFB",
         }
       },
       boxShadow: {
         "l-form": "2.5px 3.5px 3.5px rgb(15, 15,15,.15), -2.5px -2.5px 3.5px rgb(15, 15,15,.15)",
-        "d-form": "2.5px 2.5px 3.5px rgb(235, 235,235,.15), -2.5px -2.5px 3.5px rgb(235, 235,235,.15)"
+        "d-form": "1.5px 2.5px 4.5px rgb(235, 235,235,.25), -1.5px -1.5px 4.5px rgb(235, 235,235,.25)"
       },
       screens: {
         "md": { "min": "425px", "max": "759px" },
         "lg": { "min": "760px", "max": "1199px" },
         "xlg": { "min": "1200px" }
+      },
+      backgroundImage: {
+        'professor': "url('/4093887.jpg')",
+        "student": "url('/8600.jpg')"
       }
     },
   },
